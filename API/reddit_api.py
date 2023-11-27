509b86dc7487ef5d557eaae421ce05519b0844da
s2545195 <a.rusu-1@student.utwente.nl>
2023-11-27 21:19:32 +0100

    API Works with comments as well

--- API/reddit_api.py
+++ API/reddit_api.py
@@ -1,23 +1,61 @@
-from flask import Flask, jsonify, request
-import requests
+import praw
+from praw.models import MoreComments
+import csv
+import pandas as pd
 
-app = Flask(__name__)
+data_points = 0
 
-@app.route('/get_reddit_data', methods=['GET'])
-def get_reddit_data():
-    topic = request.args.get('topic', default='', type=str)
-    if not topic:
-        return jsonify({"error": "Topic parameter is required"}), 400
+user_info = open('userInfo.txt', 'r')
+lines = user_info.readlines()
 
-    # Call Pushshift API to get Reddit data based on the chosen topic
-    url = f'https://api.pushshift.io/reddit/search/submission/?q={topic}&size=10'
-    response = requests.get(url)
-    data = response.json()
+client_id = 'YYr-1yy7SSBNFKDfRBLvnA'
+client_secret = '-i0blTrr4eoGSi_H8GfGAbb-AiaFPQ'
+user_agent = 'rusuMercedesBenz'
 
-    # Extract relevant information from the response
-    relevant_data = [{'title': post['title'], 'url': post['url']} for post in data['data']]
+user_info.close()
 
-    return jsonify(relevant_data)
+broad_search_terms = ['geoengineering', 'geo-engineering', 'solar radiation management', 'carbon capture',
+                      'carbon capture and storage', 'stratospheric aerosol injection',
+                      'direct air capture', 'co2 removal']
+reddit = praw.Reddit(client_id=client_id, client_secret=client_secret, user_agent=user_agent)
+csv_data = [['ID', 'Title', 'Author', 'Score', 'Number of Comments', 'Text', 'Topic']]
 
-if __name__ == '__main__':
-    app.run(debug=True)
+
+def get_comments(comments, passed_csv, data_point=data_points):
+    index = 0
+    for comment in comments:
+        if not isinstance(comment, MoreComments) and not comment.is_submitter and not comment.author == '':
+            passed_csv.append([comment.id, comment.parent_id, comment.author, comment.score, comment.replies.__len__(),
+                           comment.body, 'COMMENT'])
+            index += 1
+            data_point += 1
+            print(str(data_point) + ' C')
+
+        if index > 9:
+            break
+
+
+def search_reddit(title_includes, data_point=data_points):
+    search_results = reddit.subreddit("all").search(title_includes, sort='relevance', time_filter='all', limit=50)
+    for submission in search_results:
+        csv_data.append([submission.id, submission.title, submission.author.name, submission.score,
+                         submission.num_comments, submission.selftext, title_includes.upper()])
+        get_comments(submission.comments.list(), csv_data)
+        data_point += 1
+
+
+for topic in broad_search_terms:
+    search_reddit(topic)
+
+csv_file_path = 'reddit_geoengineering_posts.csv'
+with open(csv_file_path, 'w', newline='', encoding='utf-8') as csvfile:
+    csv_writer = csv.writer(csvfile)
+    csv_writer.writerows(csv_data)
+
+df = pd.read_csv(csv_file_path)
+
+df_no_duplicates = df.drop_duplicates(subset='ID')
+df_no_duplicates = df.dropna(subset=['Topic'])
+df_no_duplicates.to_csv(csv_file_path, index=False, encoding='utf-8')
+
+print(f"Search results for '{broad_search_terms}' saved to {csv_file_path}")
